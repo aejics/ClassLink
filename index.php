e13384e2a0acb4aa856f66263136a63c52d1cf4e
Marco Pisco <marco@marcopisco.com>
2025-01-26 23:13:40 +0000

    test code

--- index.php
+++ index.php
@@ -5,11 +5,14 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Page Title</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
 </head>
 <body>
-    <?php
-        require_once(__DIR__ . "/vendor/autoload.php");
-        echo "Hello World!";
-    ?>
+    <form action="login.php" method="POST">
+    Cartao: <input type="text" name="user"><br>
+    Pass: <input type="password" name="pass"><br>
+    <input type="submit">
+</form>
 </body>
 </html>
