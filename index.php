38c42521566853197f0e21f86635266e97237e58
Marco Pisco <a11531@aejics.org>
2025-01-27 16:59:37 +0000

    Adicionar estrelas de required

--- index.php
+++ index.php
@@ -29,11 +29,11 @@
                 <p class="mb-4">Utilize as credenciais do GIAE para continuar para Reserva Salas</p>
                 <form action="login.php" method="POST" class="w-100" style="max-width: 300px;">
                     <div class="mb-3">
-                        <label for="user" class="form-label">Nome de utilizador:</label>
+                        <label for="user" class="form-label">Nome de utilizador <b class="required">*</b>:</label>
                         <input type="text" class="form-control" id="user" name="user" required autofocus placeholder="f1964">
                     </div>
                     <div class="mb-3">
-                        <label for="pass" class="form-label">Palavra Passe:</label>
+                        <label for="pass" class="form-label">Palavra-passe <b class="required">*</b>:</label>
                         <input type="password" class="form-control" id="pass" name="pass" required placeholder="********">
                     </div>
                     <button type="submit" class="btn btn-primary w-100">Iniciar sessão</button>
