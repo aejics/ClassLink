5d0d6f4e29d0f067944ac2bc56b03f76bb524862
Marco Pisco <marco@marcopisco.com>
2025-01-27 01:31:13 +0000

    Código que funciona!!!!

--- login.php
+++ login.php
@@ -7,10 +7,25 @@
     <meta name='viewport' content='width=device-width, initial-scale=1'>
 </head>
 <body>
+    <style>
+        img {
+            width: 100px;
+            height: 150px;
+            border-radius: 50%;
+        }
+    </style>
     <?php
         require_once(__DIR__ . "/vendor/autoload.php");
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
         $giae = new \juoum\GiaeConnect\GiaeConnect("giae.aejics.org", $_POST["user"], $_POST["pass"]);
-        print_r($giae->getSaldo());
+        $config = json_decode($giae->getConfInfo(), true);
+        $nome = $config['nomeutilizador'];
+        $fotoutente = json_decode('"' . $config['fotoutente'] . '"'); // Decode Unicode
+        echo "<img src='https://giae.aejics.org/" . $fotoutente . "' />";
     ?>
+
+    <h1>Bem-vindo, <?php echo($nome)?></h1>
 </body>
 </html>
