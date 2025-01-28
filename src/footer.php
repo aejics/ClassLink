a8567776c5718c4ffb70f368d1ddd9b2b3a0bc80
Marco Pisco <a11531@aejics.org>
2025-01-28 22:31:42 +0000

    Arranjar bug do ano não estar no footer

--- src/footer.php
+++ src/footer.php
@@ -1,4 +1,4 @@
 <?php echo "
 <div class='h-100 d-flex align-items-center justify-content-center flex-column'>
-<p class='font-weight-light'><small>(C) <?php echo date('Y'); ?> 2ºEF Agrupamento de Escolas Joaquim Inácio da Cruz Sobral</small></p>
+<p class='font-weight-light'><small>(C) " . date('Y') . " 2ºEF Agrupamento de Escolas Joaquim Inácio da Cruz Sobral</small></p>
 </div>" ?>
