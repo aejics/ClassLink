65892c4a86a84f0b8fff4f2de5563cf5db04ca2e
Marco Pisco <a11531@aejics.org>
2025-02-03 16:39:54 +0000

    Arranjar espaço para perfeicionar a página de login

--- src/footer.php
+++ src/footer.php
@@ -4,4 +4,4 @@ function getLatestCommit() {
 }
 
 echo "<hr><div class='h-100 d-flex align-items-center justify-content-center flex-column'>
-<p class='font-weight-light'><small>(C) " . date('Y') . " AEJICS (2ºEF)- <a href='https://github.com/aejics/reservasalas/commit/" . getLatestCommit() ."'> Versão " . getLatestCommit() . "</a></i></small></p>" ?>
+<p class='font-weight-light'><small>(C) " . date('Y') . " AEJICS (2ºEF) - <a href='https://github.com/aejics/reservasalas/commit/" . getLatestCommit() ."'>Versão " . getLatestCommit() . "</a></i></small></p>" ?>
