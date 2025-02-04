98d1ac356951cabe12b5598cb5b6c94be7b27352
Marco Pisco <a11531@aejics.org>
2025-02-04 16:12:23 +0000

    começar a implementar sql no index

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     ini_set('display_errors', 'On');
     $db = new SQLite3('db.sqlite3');
     $id = filter_input(INPUT_COOKIE, 'user', FILTER_UNSAFE_RAW);
+    $db->exec("CREATE TABLE tempos (idtempo INTEGER, horastart TIME, horaend TIME, PRIMARY KEY (idtempo);");
+        $db->exec("CREATE TABLE reservas (idtempo INTEGER, data DATE, reservador VARCHAR(10), razao VARCHAR(999), aprovado BOOL, aprovadopor VARCHAR(10) PRIMARY KEY (idtempo, data), FOREIGN KEY (idtempo) REFERENCES tempos(idtempo), FOREIGN KEY (reservador) REFERENCES cache_giae(id), FOREIGN KEY (aprovadopor) REFERENCES cache_giae(id));");
     $nome = $db->querySingle("SELECT nome from cache_giae WHERE id='{$id}';");
     if (isset($_COOKIE["loggedin"])) {
         echo("<div class='h-100 d-flex align-items-center justify-content-center flex-column'>
