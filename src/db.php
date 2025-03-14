95b0aa59d3a0b73d40375c1d35d46c057a2d9fe8
Marco Pisco <a11531@aejics.org>
2025-03-14 00:35:30 +0000

    task: remover debug

--- src/db.php
+++ src/db.php
@@ -1,9 +1,7 @@
 <?php
     require 'config.php';
-    //talvez isto seja mudado mais tarde
     $db = new mysqli($db['servidor'], $db['user'], $db['password'], $db['db'], $db['porta']);
-    echo $db->host_info . "\n";
     if ($db->connect_error) {
-        die("Connection failed: " . $db->connect_error);
+        die("Ligação ao servidor falhou: " . $db->connect_error);
     }
 ?>
