dfffa313336df0559d1e6c27175f9cc95ec660b4
Marco Pisco <a11531@aejics.org>
2025-03-15 19:39:28 +0000

    task: começar a implementar gestão de pedidos no administrativo

--- admin/pedidos.php
+++ admin/pedidos.php
@@ -3,8 +3,7 @@
 ?>
 <div class="h-100 d-flex align-items-center justify-content-center flex-column">
     <h3>Gestão de Pedidos de Salas</h3>
-    <p class="h2 fw-light">Reservar uma Sala</p>    
-<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="POST" class="d-flex align-items-center">
+<form action="/admin/pedidos.php" method="POST" class="d-flex align-items-center">
     <div class="form-floating me-2">
         <select class="form-select" id="sala" name="sala" required onchange="this.form.submit();">
             <?php if ($_POST['sala'] == "0" | !$_POST['sala']){
@@ -24,78 +23,63 @@
         </select>
         <label for="sala" class="form-label">Escolha uma sala</label>
     </div>
+    <div class="form-floating me-2">
+        <input type="text" class="form-control form-control-sm" id="requisitor" name="requisitor" placeholder="Requisitor" value="">
+        <label for="requisitor">Requisitor</label>
+    </div>
 </form>
-</div>
+
 <?php
-    if ($_POST['sala'] | $_GET['sala']){
-        echo(
-            "<div class='mt-3 h-100 d-flex align-items-center justify-content-center flex-column'>
-            <table class='table table-bordered'><thead><tr><th scope='col'>Tempos</th>"
-        );
-        for ($i = 0; $i < 7; $i++){
-            if ($_GET['before']){
-                $segunda = strtotime($_GET['before']);
-            } else {
-                $segunda = strtotime("monday this week");
-            }
-            $segundadiaantes = strtotime("-1 week", $segunda);
-            $segundadiaantes = date("d-m-Y", $segundadiaantes);
-            $segundadiadepois = strtotime("+1 week", $segunda);
-            $segundadiadepois = date("d-m-Y", $segundadiadepois);
-            $dia = date("d-m-Y", strtotime("+{$i} day", $segunda));
-            echo "<th scope='col'>{$dia}</th>";
-        };
-        echo "</tr></thead><tbody>";
-        $tempos = $db->query("SELECT * FROM tempos;");
-        // por cada tempo:
-        for ($i = 1; $i <= $tempos->num_rows; $i++){
-            while ($row = $tempos->fetch_assoc()){
-                echo "<tr><th scope='row'>{$row['horashumanos']}</td>";
-                // por cada dia da semana:
-                for ($j = 0; $j < 7; $j++){
-                    $diacheckdb = $segunda + ($j * 86400);
-                    $diacheckdb = date("Y-m-d", $diacheckdb);
-                    if ($_GET['sala']){
-                        $tempoatualdb = $db->query("SELECT * FROM reservas WHERE sala='{$_GET['sala']}' AND data='{$diacheckdb}' AND tempo='{$row['id']}';");
-                        $sala = $_GET['sala'];
-                    } else {
-                        $tempoatualdb = $db->query("SELECT * FROM reservas WHERE sala='{$_POST['sala']}' AND data='{$diacheckdb}' AND tempo='{$row['id']}';");
-                        $sala = $_POST['sala'];
-                    }
-                    $tempoatualdb = $tempoatualdb->fetch_assoc();
-                    if (!$tempoatualdb){
-                        echo "<td class='bg-success text-white text-center'>
-                        <a href='/reservas/manage.php?tempo={$row['id']}&sala={$sala}&data={$diacheckdb}'>
-                        Livre
-                        </a></td>";
-                    } else { 
-                        $nomerequisitor = $db->query("SELECT nome FROM cache_giae WHERE id='{$tempoatualdb['requisitor']}';");
-                        $nomerequisitor = $nomerequisitor->fetch_assoc();
-                        if ($tempoatualdb['aprovado'] == 0){
-                            echo "<td class='bg-warning text-white text-center'>
-                            <a href='/reservas/manage.php?tempo={$row['id']}&sala={$sala}&data={$diacheckdb}'>
-                            Pendente
-                            <br>
-                            {$nomerequisitor['nome']}
-                            </a></td>";
-                        } else if ($tempoatualdb['aprovado'] == 1){
-                            echo "<td class='bg-danger text-white text-center'>
-                            <a href='/reservas/manage.php?tempo={$row['id']}&sala={$sala}&data={$diacheckdb}'>
-                            Ocupado
-                            <br>
-                            {$nomerequisitor['nome']}
-                            </a></td>";
-                    }
-                }
-            }
-            echo "</tr>";
-            }
+    if ($_GET['subaction']) {
+        switch ($_GET['subaction']){
+            case "aprovar":
+                $db->query("UPDATE reservas SET aprovado=1 WHERE sala='{$_GET['sala']}' AND tempo='{$_GET['tempo']}' AND data='{$_GET['data']}';");
+                echo "<div class='mt-2 alert alert-success fade show' role='alert'>Reserva aprovada com sucesso.</div>";
+                echo "<a href='/admin/pedidos.php'><button class='btn btn-primary'>Voltar</button></a>";
+                break;
+            case "rejeitar":
+                $db->query("DELETE FROM reservas WHERE sala='{$sala}' AND tempo='{$tempo}' AND data='{$data}';");
+                header("Location: /reservas/?sala={$sala}");
+                break;
+            case "detalhes":
+                
         }
-        echo "</table><div class='d-flex'><a href='/reservas/?before={$segundadiaantes}&sala=";
+    } elseif ($_POST['sala'] | $_GET['sala']){
+        echo "<div style='max-height: 400px; overflow-y: auto; width: 100%;'>";
+        echo "<table class='table'><tr><th scope='col'>Data</th><th scope='col'>Tempo</th><th scope='col'>Requisitor</th><th scope='col'>Motivo</th><th scope='col'>AÇÕES</th></tr>";
         if ($_POST['sala']){
-            echo "{$_POST['sala']}' class='btn mb-2 me-2 btn-success'>Semana Anterior</a> <a href='/reservas/?before={$segundadiadepois}&sala={$_POST['sala']}' class='btn mb-2 ms-2 btn-success'>Semana Seguinte</a></div></div>";
+            $sala = $_POST['sala'];
         } else {
-            echo "{$_GET['sala']}' class='btn mb-2 me-2 btn-success'>Semana Anterior</a> <a href='/reservas/?before={$segundadiadepois}&sala={$_GET['sala']}' class='btn mb-2 ms-2 btn-success'>Semana Seguinte</a></div></div>";
+            $sala = $_GET['sala'];
         }
+        if ($_POST['requisitor']){ 
+            $pedidos = $db->query("SELECT * FROM reservas WHERE aprovado=0 AND sala={$sala} AND requisitor='{$_POST['requisitor']}' ORDER BY data ASC;");
+            while ($pedido = $pedidos->fetch_assoc()){
+                $salaextenso = $db->query("SELECT nome FROM salas WHERE id='{$pedido['sala']}';")->fetch_assoc()['nome'];
+                $requisitorextenso = $db->query("SELECT nomecompleto FROM cache_giae WHERE id='{$pedido['requisitor']}';")->fetch_assoc()['nomecompleto'];
+                $horastempo = $db->query("SELECT horashumanos FROM tempos WHERE id='{$pedido['tempo']}';")->fetch_assoc()['horashumanos'];
+                echo "<tr><td>{$pedido['data']}</td>
+                <td>{$horastempo}</td>
+                <td>{$requisitorextenso}</td>
+                <td>{$pedido['motivo']}</td>
+                <td><a href='/reservas/manage.php?subaction=reservar&tempo={$pedido['tempo']}&data={$pedido['data']}&sala={$pedido['sala']}' class='btn btn-success'>Aprovar</a> <a href='/reservas/manage.php?subaction=delete&tempo={$pedido['tempo']}&data={$pedido['data']}&sala={$pedido['sala']}' class='btn btn-danger'>Rejeitar</a></td></tr>";
+            }    
+        } else {
+            $pedidos = $db->query("SELECT * FROM reservas WHERE aprovado=0 AND sala={$sala};");
+            while ($pedido = $pedidos->fetch_assoc()){
+                $salaextenso = $db->query("SELECT nome FROM salas WHERE id='{$pedido['sala']}';")->fetch_assoc()['nome'];
+                $requisitorextenso = $db->query("SELECT nomecompleto FROM cache_giae WHERE id='{$pedido['requisitor']}';")->fetch_assoc()['nomecompleto'];
+                $horastempo = $db->query("SELECT horashumanos FROM tempos WHERE id='{$pedido['tempo']}';")->fetch_assoc()['horashumanos'];
+                echo "<tr><td>{$pedido['data']}</td>
+                <td>{$horastempo}</td>
+                <td>{$requisitorextenso}</td>
+                <td>{$pedido['motivo']}</td>
+                <td><a href='/admin/pedidos.php?subaction=aprovar&tempo={$pedido['tempo']}&data={$pedido['data']}&sala={$pedido['sala']}' class='btn btn-success'>Aprovar</a>
+                 <a href='/admin/pedidos.php?subaction=rejeitar&tempo={$pedido['tempo']}&data={$pedido['data']}&sala={$pedido['sala']}' class='btn btn-danger'>Rejeitar</a>
+                 <a href='/reservas/manage.php?&tempo={$pedido['tempo']}&data={$pedido['data']}&sala={$pedido['sala']}' class='btn btn-secondary'>Detalhes</a></td></tr>";
+            }
+        }
+        echo "</table>";
+        echo "</div>";
     }
 ?>
