e4b0010b6887c283837fdc90769615742b1a2227
Marco Pisco <a11531@aejics.org>
2025-03-29 19:40:40 +0000

    Remover script de javascript inútil

--- login/logincss.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.btnCardRegis ,.btnCardLogin').click(function() {
-        $('.cardLogin').toggleClass('off');
-        $('.cardRegis').toggleClass('on');
-        $('.btnIcon').toggleClass('rotate');
-        $('.sosmed').toggleClass('top');
-      })
-});
