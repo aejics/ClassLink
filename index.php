a162036647915c2f05543972817a5c78d7c65937
Marco Pisco <a11531@aejics.org>
2025-03-29 19:16:01 +0000

    Nunca vos ensinaram que é rude falar em caps!?

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
         </div>
     </nav>
     <div class="text">
-        <h3>BEM VINDO, <?php echo "{$dados['nome']}"; ?>, à <br> <span>Reserva De Salas</span> </h3>
+        <h3>Seja bem vindo, <?php echo "{$dados['nome']}"; ?>, à <br> <span>Reserva De Salas</span> </h3>
         <p>Projeto elaborado pela turma 2ºE e 1ºD 2024/25</p>
         <a href="/reservar">
             <button class="btn">
