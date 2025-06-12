ae3f5204a9911ea8ffd9183ade75a66e30b75764
Marco Pisco <a11531@aejics.org>
2025-06-12 08:40:00 +0100

    implementar popup de ter a certeza de ação perigosa

---
+++ admin/sample.php
@@ -0,0 +1,8 @@
+<?php // tenho que importar o jquery ?>
+<a href="/DoSomething" onclick="return confirm('Tem a certeza que pretende x?');">Link</a>
+
+<?php
+    require 'index.php';
+
+    echo("teste");
+?>
