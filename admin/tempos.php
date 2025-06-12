5fab5f96bcc0b8acb8912f137bc352caab16434c
Marco Pisco <a11531@aejics.org>
2025-06-12 08:33:58 +0100

    várias alterações aos tempos:
    - passar a usar uuids para gerar os tempos, assim o utilizador não tem que fazer mais nada sem ser indicar as horas
    - passar a ordenar automaticamente pela ordem, pois o sql entende o formato do tempo
    - (não sei qual é a minha alteração ao login, mas parece me ser uma linha que apaguei sem querer. continua a funcionar, por tanto whatever).

--- admin/tempos.php
+++ admin/tempos.php
@@ -3,7 +3,6 @@
 <div class="d-flex align-items-center mb-3">
     <span class="me-3">Adicionar um tempo</span>
     <?php formulario("tempos.php?action=criar", [
-        ["type" => "number", "id" => "id", "placeholder" => "ID do Tempo", "label" => "ID do Tempo", "value" => null],
         ["type" => "text", "id" => "horahumana", "placeholder" => "Horas (08:05-08:55)", "label" => "Horas (08:05-08:55)", "value" => null]
     ]); ?>
 </div>
@@ -12,7 +11,8 @@
 switch ($_GET['action']){
     // caso seja preenchido o formulário de criação:
     case "criar":
-        $db->query("INSERT INTO tempos (id, horashumanos) VALUES ('{$_POST["id"]}', '{$_POST["horahumana"]}');");
+        $randomuuid = uuid4();
+        $db->query("INSERT INTO tempos (id, horashumanos) VALUES ('{$randomuuid}', '{$_POST["horahumana"]}');");
         acaoexecutada("Criação de Tempo");
         break;
     // caso execute a ação apagar:
@@ -24,26 +24,25 @@ switch ($_GET['action']){
     case "edit":
         $c = $db->query("SELECT * FROM tempos WHERE id = '{$_GET['id']}';");
         $d = $c->fetch_assoc();
-        echo "<div class='alert alert-warning fade show' role='alert'>A editar o Tempo {$d['id']} (<b>{$d['horashumanos']}</b>).</div>";
+        echo "<div class='alert alert-warning fade show' role='alert'>A editar o Tempo <b>{$d['horashumanos']}</b>.</div>";
         formulario("tempos.php?action=update&id={$d['id']}", [
-            ["type" => "number", "id" => "id", "placeholder" => "ID do Tempo", "label" => "ID do Tempo", "value" => $d['id']],
             ["type" => "text", "id" => "horahumana", "placeholder" => "Horas (08:05-08:55)", "label" => "Horas (08:05-08:55)", "value" => $d['horashumanos']]]);
         break;
     // caso seja submetida a edição:
     case "update":
-        $db->query("UPDATE tempos SET id = '{$_POST['id']}', horashumanos = '{$_POST['horahumana']}' WHERE id = {$_GET['id']};");
+        $db->query("UPDATE tempos SET horashumanos = '{$_POST['horahumana']}' WHERE id = {$_GET['id']};");
         acaoexecutada("Atualização de Tempo");
         break;
 }
 
-$temposatuais = $db->query("SELECT * FROM tempos;");
+$temposatuais = $db->query("SELECT * FROM tempos ORDER BY horashumanos ASC;");
 if ($temposatuais->num_rows == 0) {
     echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>Não existem tempos.</div>\n";
 }
 echo "<div style='max-height: 400px; overflow-y: auto; width: 90%;'>";
-echo "<table class='table'><tr><th scope='col'>ID</th><th scope='col'>Hora Humana</th><th scope='col'>AÇÕES</th></tr>";
+echo "<table class='table'><tr><th scope='col'>Hora Humana</th><th scope='col'>AÇÕES</th></tr>";
 while ($row = $temposatuais->fetch_assoc()) {
-    echo "<tr><td>{$row['id']}</td><td>{$row['horashumanos']}</td><td><a href='/admin/tempos.php?action=edit&id={$row['id']}'>EDITAR</a>  <a href='/admin/tempos.php?action=apagar&id={$row['id']}'>APAGAR</a></tr>";
+    echo "<tr><td>{$row['horashumanos']}</td><td><a href='/admin/tempos.php?action=edit&id={$row['id']}'>EDITAR</a>  <a href='/admin/tempos.php?action=apagar&id={$row['id']}'>APAGAR</a></tr>";
 }
 echo "</div></table>"
 ?>
