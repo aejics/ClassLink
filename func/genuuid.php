68ff85649d5bbf1b3b9dfa86bced429e305cd60d
Marco Pisco <a11531@aejics.org>
2025-06-12 08:17:21 +0100

    começar a implementar a geração de uuids

---
+++ func/genuuid.php
@@ -0,0 +1,14 @@
+<?php
+// Esta função gera UUIDs V4, que são compliant com a especificação que é suposta ser utilizada.
+// Todos os IDs eventualmente acabam por usar esta função.
+
+function uuidv4()
+{
+  $data = random_bytes(16);
+
+  $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+  $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+    
+  return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
+?>
