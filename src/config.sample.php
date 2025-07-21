54180ebf1624d19bb171e21d28b4115b5a85c404
Marco Pisco <a11531@aejics.org>
2025-07-21 11:03:56 +0100

    bloquear o config.php de sincronizar

---
+++ src/config.sample.php
@@ -0,0 +1,28 @@
+<?php
+    $info = array(
+        'adminforcado' => 'a11531',
+        'giae' => 'giae.aejics.org'
+    );
+
+    $mail = array(
+        'servidor' => 'smtp.gmail.com',
+        'porta' => 465,
+        'autenticacao' => true,
+        // caso a autenticação seja por starttls, usar PHPMailer::ENCRYPTION_STARTTLS
+        // caso a autenticação seja por ssl, usar PHPMailer::ENCRYPTION_SMTPS
+        // caso não seja necessário autenticação, por false na opção autenticacao, e não importar-se para os outros
+        'tipodeseguranca' => 'PHPMailer::ENCRYPTION_STARTTLS ou PHPMailer::ENCRYPTION_SMTPS',
+        'mail' => '',
+        'password' => ''
+    );
+
+    // A base de dados deste projeto é MySQL.
+    $db = array(
+        'tipo' => 'mysql',
+        'servidor' => 'localhost',
+        'user' => 'salasuser',
+        'password' => 'salaspass',
+        'db' => 'reservasalas',
+        'porta' => 3306
+    );
+?>
