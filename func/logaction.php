ab547a7f6b54008447f68bdf19fc63447bdd87b4
Marco Pisco <marco@marcopisco.com>
2025-09-05 19:52:14 +0100

    fix logaction, estava a enviar valores errados do sistema de tokens antigo

--- func/logaction.php
+++ func/logaction.php
@@ -1,21 +1,10 @@
 <?php
-function logaction(string $loginfo, string $tokenid){
+function logaction(string $loginfo, string $userid){
     require_once(__DIR__ . '/../func/genuuid.php');
     require_once(__DIR__ . "/../src/db.php");
     global $db;
 
     $id = uuid4();
-    // prepare statement
-    $horaatual = time();
-    $tempoatual > $validotill;
-    $whosetoken = $db->query("SELECT id FROM tokens WHERE validotill > '". $horaatual ."' AND token = '" . $tokenid . "';");
-    $userid = null;
-    if ($whosetoken && $whosetoken->num_rows > 0) {
-        $row = $whosetoken->fetch_assoc();
-        $userid = $row['id'];
-    }
-
-    // enviar o statement
     $stmt = $db->prepare("INSERT INTO logs (id, loginfo, userid) VALUES (?, ?, ?)");
     $stmt->bind_param("sss", $id, $loginfo, $userid);
     $stmt->execute();
