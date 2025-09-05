ad2f03458c17654ebb738f29f52416dd936c88f2
Marco Pisco <marco@marcopisco.com>
2025-09-05 19:57:34 +0100

    Trazer config de mail de volta.

--- src/config.sample.php
+++ src/config.sample.php
@@ -2,6 +2,19 @@
     require_once(__DIR__ . '/../vendor/autoload.php');
     use League\OAuth2\Client\Provider\GenericProvider;
 
+    $mail = array(
+        'ativado' => false,
+        'servidor' => 'smtp.gmail.com',
+        'porta' => 465,
+        'autenticacao' => true,
+        // caso a autenticação seja por starttls, usar PHPMailer::ENCRYPTION_STARTTLS
+        // caso a autenticação seja por ssl, usar PHPMailer::ENCRYPTION_SMTPS
+        // caso não seja necessário autenticação, por false na opção autenticacao, e não importar-se para os outros
+        'tipodeseguranca' => 'PHPMailer::ENCRYPTION_STARTTLS ou PHPMailer::ENCRYPTION_SMTPS',
+        'mail' => '',
+        'password' => ''
+    );
+    
     // A base de dados deste projeto é MySQL.
     $db = array(
         'tipo' => 'mysql',
